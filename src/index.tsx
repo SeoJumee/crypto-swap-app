3b89feebc28022d26877b2ea9c6aa1c61ea5cd85
SeoJumee <official.jumee@gmail.com>
2024-06-13 10:54:03 +0900

    Add 스타일 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalStyles } from './GlobalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -18,6 +19,7 @@ const queryClient = new QueryClient({
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
