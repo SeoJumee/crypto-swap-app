fb5235445fb3263b3fdbd908188d9e319f3cc3e5
SeoJumee <official.jumee@gmail.com>
2024-06-14 09:33:15 +0900

    Add 암호화폐 수량 입력 소수점 자리 제한 기능 구현

--- src/components/CryptoInputList/CryptoInput/index.tsx
+++ src/components/CryptoInputList/CryptoInput/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import TokenSelectButton from '../TokenSelectButton';
 import * as S from './style';
 
 function CryptoInput() {
+  const [inputValue, setInputValue] = useState('');
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    if (/^\d*\.?\d{0,10}$/.test(value)) {
+      setInputValue(value);
+    }
+  }
+
   return (
     <S.CryptoInputWrapper>
       <S.CryptoInput
         type="number"
         placeholder="0.0"
         onWheel={(event) => (event.target as HTMLElement).blur()}
+        onChange={handleInputChange}
+        value={inputValue}
       />
       <TokenSelectButton />
     </S.CryptoInputWrapper>
